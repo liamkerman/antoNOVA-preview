5524b1099e46c0f42f2a7077a4ee4a63c3432436
Viliam Vakerman <viliam.vakerman@gmail.com>
2025-11-15 14:01:40 +0100

    Update preview build - 2025-11-15T13:01:40.348Z

---
+++ assets/gdpr.component-B0gtLl8u.js
@@ -0,0 +1,18 @@
+import{B as p,S as h}from"./site-wrapper.component-CFmx-GGJ.js";import{R as g,b as d,C as l,S as m,f as a,y as o,h as t,k as e,L as r}from"./index-nzJ9nklq.js";const v="Ochrana osobných údajov (GDPR)",u="Spracovanie osobných údajov",f="V súlade s nariadením Európskeho parlamentu a Rady (EÚ) 2016/679 z 27. apríla 2016 o ochrane fyzických osôb pri spracúvaní osobných údajov a o voľnom pohybe takýchto údajov (GDPR) vás informujeme o spracúvaní vašich osobných údajov.",_="Správca osobných údajov",P="Správcom osobných údajov je autoškola antoNOVA. Kontaktné údaje nájdete v sekcii Kontakt.",b="Účel spracovania",y="Vaše osobné údaje spracúvame za účelom poskytovania služieb autoškoly, komunikácie s klientmi, vedenia evidencií v súlade so zákonom a dodržiavania zákonných povinností.",C="Vaše práva",j="V súlade s GDPR máte právo na prístup k svojim osobným údajom, právo na opravu, výmaz, obmedzenie spracovania, právo na prenosnosť údajov, právo namietať spracovanie a právo podať sťažnosť Úradu na ochranu osobných údajov.",k={title:v,heading1:u,paragraph1:f,heading2:_,paragraph2:P,heading3:b,paragraph3:y,heading4:C,paragraph4:j},O="Privacy Policy (GDPR)",M="Personal Data Processing",D="In accordance with Regulation (EU) 2016/679 of the European Parliament and of the Council of 27 April 2016 on the protection of natural persons with regard to the processing of personal data and on the free movement of such data (GDPR), we inform you about the processing of your personal data.",R="Data Controller",w="The data controller is driving school antoNOVA. You can find contact details in the Contact section.",$="Purpose of Processing",z="We process your personal data for the purpose of providing driving school services, communicating with clients, maintaining records in accordance with the law, and fulfilling legal obligations.",G="Your Rights",S="In accordance with GDPR, you have the right to access your personal data, the right to rectification, erasure, restriction of processing, the right to data portability, the right to object to processing, and the right to lodge a complaint with the Data Protection Authority.",V={title:O,heading1:M,paragraph1:D,heading2:R,paragraph2:w,heading3:$,paragraph3:z,heading4:G,paragraph4:S};class i extends p{t=this.registerTranslations({sk:k,en:V});static ɵfac=(()=>{let s;return function(n){return(s||(s=g(i)))(n||i)}})();static ɵcmp=d({type:i,selectors:[["app-gdpr"]],features:[m],decls:21,vars:9,consts:[[1,"content"]],template:function(c,n){c&1&&(a(0,"app-site-wrapper")(1,"section")(2,"h1"),o(3),t(),a(4,"div",0)(5,"h2"),o(6),t(),a(7,"p"),o(8),t(),a(9,"h2"),o(10),t(),a(11,"p"),o(12),t(),a(13,"h2"),o(14),t(),a(15,"p"),o(16),t(),a(17,"h2"),o(18),t(),a(19,"p"),o(20),t()()()()),c&2&&(e(3),r(n.t("title")),e(3),r(n.t("heading1")),e(2),r(n.t("paragraph1")),e(2),r(n.t("heading2")),e(2),r(n.t("paragraph2")),e(2),r(n.t("heading3")),e(2),r(n.t("paragraph3")),e(2),r(n.t("heading4")),e(2),r(n.t("paragraph4")))},dependencies:[l,h],styles:[`section[_ngcontent-%COMP%] {
+  padding: 2rem 0;
+}
+section[_ngcontent-%COMP%]   h1[_ngcontent-%COMP%] {
+  margin-bottom: 2rem;
+}
+section[_ngcontent-%COMP%]   .content[_ngcontent-%COMP%]   h2[_ngcontent-%COMP%] {
+  margin-top: 2rem;
+  margin-bottom: 1rem;
+  font-size: 1.5rem;
+}
+section[_ngcontent-%COMP%]   .content[_ngcontent-%COMP%]   p[_ngcontent-%COMP%] {
+  margin-bottom: 1.5rem;
+  line-height: 1.6;
+}
+section[_ngcontent-%COMP%]   .content[_ngcontent-%COMP%]   h2[_ngcontent-%COMP%]:first-of-type {
+  margin-top: 0;
+}`],changeDetection:0})}export{i as GdprComponent};
